252248e0837d7adc051bede80b11a7e3fecd0cf9
Raquel-bena <raquelbdv56@gmail.com>
2026-01-05 21:02:53 +0100

    refactor: force rename detection for Emotions-000

---
+++ _archive/prototypes/in-transit/config/secrets.js
@@ -0,0 +1,13 @@
+// config/secrets.js
+// NO COMPARTIR ESTE ARCHIVO PÚBLICAMENTE
+
+// Clima (OpenWeatherMap)
+const OWM_KEY = '09a95abe51374eae766a284a97a3f039'; 
+const OWM_CITY_ID = 3128740; // Barcelona ID
+
+// Transporte (TMB Barcelona)
+const TMB_APP_ID = 'daf62db0';
+const TMB_APP_KEY = '5e4adb21bdfeda65a91e36cc2c12b7df';
+
+// Ambiente (SmartCitizen - MACBA)
+const SC_DEVICE_ID = 16559;
